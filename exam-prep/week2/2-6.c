31c7d8df84ce488cd0b943fe1484301c4be84248
Soo <soojlee0106@naver.com>
2023-04-20 12:40:47 +0900

    Add difference between call by value and reference

--- exam-prep/week2/2-6.c
+++ exam-prep/week2/2-6.c
@@ -22,3 +22,26 @@ int main()
     printf("%d", p->age);      // 포인터를 사용해서 값 읽어오기. %d is a format specifier for int.
     printf("%f\n", p->salary); // 포인터를 사용해서 값 읽어오기. %f is a format specifier for float.
 }
+
+// The main difference between call by value and call by reference is that in call by value, a copy of the value of the argument is passed to the function, while in call by reference, the memory address of the argument is passed to the function.
+// call by value
+void increment(int x)
+{
+    x++;
+}
+
+int call_by_value()
+{
+    int num = 10;
+    increment(num);
+    printf("%d", num); // output: 10. In call by value, a copy of the value of the argument is passed to the function. Any changes made to the parameter within the function do not affect the original value of the argument passed from outside the function.
+    return 0;
+}
+
+int call_by_reference()
+{
+    int num = 10;
+    increment(&num);
+    printf("%d", num); // output: 11. On the other hand, in call by reference, the memory address of the argument is passed to the function, and any changes made to the parameter within the function affect the original value of the argument passed from outside the function.
+    return 0;
+}
