0e43c83f5035a4560ca13ebcc1fde454c891c5d3
Soo <soojlee0106@naver.com>
2023-04-20 13:04:01 +0900

    Remove wrong comments

---
+++ exam-prep/week3/3-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void swap(int *x, int *y)
+{
+    int *temp; // temp is a pointer to an integer. It is not initialized, so it is pointing to an unknown location in memory.
+    temp = x;  // temp is now pointing to the same location as x.
+    x = y;     // x is now pointing to the same location as y.
+    y = temp;  // y is now pointing to the same location as temp.
+}
+
+int main()
+{
+    int a = 10, b = 20;
+    swap(&a, &b);            // a and b are passed by reference, so the values of the variables pointed to by a and b are changed.
+    printf("%d   %d", a, b); // The values of a and b are not changed. The values of the variables pointed to by a and b are changed.
+}
---
+++ exam-prep/week3/3-4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    short a[3] = {10, 20, 30};
+
+    printf("%d", a[0]);
+    printf("%d", a[1]);
+    printf("%d", *a); // a is a pointer to the first element of the array. *a is the value of the first element of the array.
+    printf("%d", *(a + 1));
+}
